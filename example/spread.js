41f5eef642a2975bf353b4597ee516aa9aa9e8fd
Kim Hyunjae <loma1016@gmail.com>
2021-05-01 23:22:07 +0900

    #6-3 Javascript Spread
    1. object spread
    2. array spread

--- example/spread.js
+++ example/spread.js
@@ -3,8 +3,18 @@
 const dog = {
   name: 'happy'
 };
+const smallDog = {
+  ...dog,
+  size: 'small'
+};
+console.log(dog);
+console.log(smallDog);
 
 // spread 문법을 이용하여 배열 합치기
 const animals = ['dog', 'cat', 'pig', 'cow'];
 const animals2 = [...animals, 'chicken'];
 const animals3 = [...animals, 'chicken', ...animals];
+
+console.log(animals);
+console.log(animals2);
+console.log(animals3);

8e944ab109b98b78357b4a923a3f51d2b733f1c9
Kim Hyunjae <loma1016@gmail.com>
2021-05-04 12:22:29 +0900

    #6-4 Javascript Spread
    + rest parameter

--- example/spread.js
+++ example/spread.js
@@ -18,3 +18,25 @@ const animals3 = [...animals, 'chicken', ...animals];
 console.log(animals);
 console.log(animals2);
 console.log(animals3);
+
+const arr1 = [1, 2];
+const arr2 = [3, 4];
+
+console.log(arr1.concat(arr2));
+console.log([...arr1, ...arr2]);
+
+const array = [1, 2, 3];
+console.log(array);
+console.log(...array);
+
+// rest parameter
+function print(...rest) {
+  console.log(rest);
+}
+print(1, 2, 3);
+
+const array = [1, 2, 3, 4, 5];
+const [first, ...rest] = array;
+
+console.log(first);
+console.log(rest);
